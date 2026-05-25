0a7a0cf7987c874af4b1751bcc1faeda1fb839bc
hishamo0 <@gmail.com>
2026-05-25 20:21:48 +0300

    update

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,15 +1,33 @@
-import { articles } from "@/data/articles";
+import { news } from "@/data/news";
 import { notFound } from "next/navigation";
+import Header from "@/components/Header";
+import Image from "next/image";
 
 type Props = {
   params: {
     slug: string;
   };
 };
+export async function generateMetadata({ params }: Props) {
 
+  const article = news.find(
+    (a) => a.slug === params.slug
+  );
+
+  if (!article) {
+    return {
+      title: "مقال غير موجود",
+    };
+  }
+
+  return {
+    title: article.title,
+    description: article.description,
+  };
+}
 export default function ArticlePage({ params }: Props) {
 
-  const article = articles.find(
+  const article = news.find(
     (a) => a.slug === params.slug
   );
 
@@ -18,16 +36,60 @@ export default function ArticlePage({ params }: Props) {
   }
 
   return (
-    <article className="max-w-4xl mx-auto px-4 py-10 text-white">
+    <main className="bg-black text-white min-h-screen">
 
-      <h1 className="text-4xl font-bold mb-6">
-        {article.title}
-      </h1>
+      <Header search="" setSearch={() => {}} />
 
-      <p className="text-zinc-300 leading-8 text-lg">
-        {article.content}
-      </p>
+      <article className="max-w-5xl mx-auto px-4 py-10">
 
-    </article>
+        <div className="relative w-full h-[500px] overflow-hidden rounded-3xl">
+
+          <Image
+            src={article.image}
+            alt={article.title}
+            fill
+            className="object-cover"
+          />
+
+        </div>
+
+        <div className="mt-8">
+
+          <span className="text-red-500 text-sm font-bold">
+            {article.category}
+          </span>
+
+          <h1 className="text-5xl font-extrabold leading-tight mt-4">
+            {article.title}
+          </h1>
+
+          <p className="text-zinc-500 mt-4">
+            {article.date}
+          </p>
+
+  <div className="mt-10">
+
+  <div
+    className="
+      prose
+      prose-invert
+      prose-h2:text-white
+      prose-p:text-zinc-200
+      prose-li:text-zinc-200
+      max-w-none
+      leading-8
+    "
+    dangerouslySetInnerHTML={{
+      __html: article.content || "",
+    }}
+  />
+
+  </div>
+
+        </div>
+
+      </article>
+
+    </main>
   );
 }
