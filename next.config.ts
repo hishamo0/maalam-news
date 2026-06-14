3b20010be80d8c0d0277ba2288f9cd0f7b851b8e
hishamo0 <@gmail.com>
2026-06-14 23:24:19 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,6 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.maalam.net",
-          },
-        ],
-        destination: "https://maalam.net/:path*",
-        permanent: true,
-      },
-      {
         source: "/article/:slug",
         destination: "/news/:slug",
         permanent: true,
